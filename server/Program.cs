a94c2ba0cdbc56e143dba55284ddedf522b4e8bb
ChaykalakDanara <chaykalak.d08@gmail.com>
2026-02-05 21:25:04 +0300

    Коммит 9 — добавление меню

--- server/Program.cs
+++ server/Program.cs
@@ -8,7 +8,15 @@ class Program
         Console.WriteLine("ФИО: Чайкалак Данара Рустамовна");
         Console.WriteLine("Группа: ИСП-231");
         Console.WriteLine($"Дата и время: {DateTime.Now}");
-       
+         while (true)
+        {
+            Console.WriteLine("\nМеню:");
+            Console.WriteLine("1 — Показать ФИО");
+            Console.WriteLine("2 — Показать группу");
+            Console.WriteLine("3 — Показать дату");
+            Console.WriteLine("4 — Выход");
+            Console.Write("Выберите пункт: ");
+            string choice = Console.ReadLine();
         }
     }
 }
