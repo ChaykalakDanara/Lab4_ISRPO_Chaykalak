549c4af97e0f6329ecdb0e3d7e8b4f4792c820b6
ChaykalakDanara <chaykalak.d08@gmail.com>
2026-02-05 21:22:13 +0300

    Коммит 8 — добавление группы и даты

--- server/Program.cs
+++ server/Program.cs
@@ -8,33 +8,7 @@ class Program
         Console.WriteLine("ФИО: Чайкалак Данара Рустамовна");
         Console.WriteLine("Группа: ИСП-231");
         Console.WriteLine($"Дата и время: {DateTime.Now}");
-        while (true)
-        {
-            Console.WriteLine("\nМеню:");
-            Console.WriteLine("1 — Показать ФИО");
-            Console.WriteLine("2 — Показать группу");
-            Console.WriteLine("3 — Показать дату");
-            Console.WriteLine("4 — Выход");
-            Console.Write("Выберите пункт: ");
-            string choice = Console.ReadLine();
-            switch (choice)
-            {
-                case "1":
-                    Console.WriteLine("ФИО: Чайкалак Данара Рустамовна");
-                    break;
-                case "2":
-                    Console.WriteLine("Группа: ИСП-231");
-                    break;
-                case "3":
-                    Console.WriteLine($"Дата и время: {DateTime.Now}");
-                    break;
-                case "4":
-                    Console.WriteLine("Выход");
-                    return;
-                default:
-                    Console.WriteLine("Неверный ввод. Попробуйте снова.");
-                    break;
-            }
+       
         }
     }
 }
